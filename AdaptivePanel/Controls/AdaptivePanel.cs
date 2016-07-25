70f3c30ae6f0b1822d2d7b0926b5ed95b75f678d
rudyhuyn <rudyhuyn@gmail.com>
2016-07-24 17:31:36 -0700

    initial commit

---
+++ AdaptivePanel/Controls/AdaptivePanel.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using System.Linq;
+
+namespace Huyn
+{
+
+
+    public enum AdaptiveBehavior { Standard = 0, StretchToMaxSizeIfPossible = 1, MinSizeOnlyIfAvailable = 2 }
+
+    public sealed class AdaptivePanel : Panel
+    {
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+
+
+            var newSize = ComputeSize(finalSize, false);
+
+            foreach (var item in Children)
+            {
+                item.Arrange(new Rect(0, 0, newSize.Width, newSize.Height));
+            }
+            return newSize;
+        }
+
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+
+
+            return ComputeSize(availableSize, true);
+        }
+
+        private Size ComputeSize(Size availableSize, bool isMeasure)
+        {
+
+
+
+            double computedWidth = 0d;
+            double computedHeight = 0d;
+
+
+            if (isMeasure)
+                foreach (var item in Children)
+                {
+                    item.Measure(availableSize);
+                }
+
+            var maxWidthChildren = Children.Select(c => c.DesiredSize.Width).Max();
+            var maxHeightChildren = Children.Select(c => c.DesiredSize.Height).Max();
+            return new Size(ComputeOneDimension(HorizontalBehavior, availableSize.Width, MinWidth, MaxWidth, maxWidthChildren, HorizontalAlignment == HorizontalAlignment.Stretch),
+                ComputeOneDimension(VerticalBehavior, availableSize.Height, MinHeight, MaxHeight, maxHeightChildren, VerticalAlignment == VerticalAlignment.Stretch));
+        }
+
+
+        private double ComputeOneDimension(AdaptiveBehavior Behavior, double availableSize, double minSize, double maxSize, double maxWidthChildren, bool isStretch)
+        {
+
+            var computedWidth = 0d;
+
+            switch (Behavior)
+            {
+                default:
+                case AdaptiveBehavior.Standard:
+                    {
+                        if (!double.IsNaN(Width))
+                            computedWidth = Width;
+                        else if (isStretch)
+                            computedWidth = Math.Min(availableSize, maxSize);
+                        else
+                        {
+                            computedWidth = Math.Max(Math.Min(Math.Min(availableSize, maxWidthChildren), maxSize), minSize);
+                        }
+                    }
+                    break;
+                case AdaptiveBehavior.StretchToMaxSizeIfPossible:
+                    {
+                        computedWidth = Math.Max(Math.Min(availableSize, maxSize), minSize);
+                    }
+                    break;
+                case AdaptiveBehavior.MinSizeOnlyIfAvailable:
+                    {
+
+                        if (!double.IsNaN(Width))
+                            computedWidth = Width;
+                        else if (isStretch)
+                            computedWidth = Math.Min(availableSize, maxSize);
+                        else
+                        {
+                            computedWidth = Math.Min(Math.Min(Math.Max(minSize, maxWidthChildren), maxSize), availableSize);
+                        }
+
+                    }
+                    break;
+            }
+            return computedWidth;
+        }
+
+
+        #region MinWidth
+
+
+
+        public double MinWidth
+        {
+            get { return (double)GetValue(MinWidthProperty); }
+            set { SetValue(MinWidthProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MinWidth.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinWidthProperty =
+            DependencyProperty.Register("MinWidth", typeof(double), typeof(AdaptivePanel), new PropertyMetadata(0d, MinWidthCallback));
+
+        private static void MinWidthCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var owner = (AdaptivePanel)d;
+            owner.MinWidth = (double)e.NewValue;
+            owner.InvalidateMeasure();
+        }
+
+
+
+        #endregion
+
+
+
+
+
+
+        #region Horizontal Fill Behavior
+        public AdaptiveBehavior HorizontalBehavior
+        {
+            get { return (AdaptiveBehavior)GetValue(HorizontalBehaviorProperty); }
+            set { SetValue(HorizontalBehaviorProperty, value); }
+        }
+
+        public static readonly DependencyProperty HorizontalBehaviorProperty =
+            DependencyProperty.Register("HorizontalBehavior", typeof(AdaptiveBehavior), typeof(AdaptivePanel), new PropertyMetadata(AdaptiveBehavior.Standard, HorizontalBehaviorCallback));
+
+        private static void HorizontalBehaviorCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            var own = (AdaptivePanel)d;
+            own.InvalidateMeasure();
+            own.InvalidateArrange();
+
+        }
+
+        #endregion
+        #region Vertical Fill Behavior
+        public AdaptiveBehavior VerticalBehavior
+        {
+            get { return (AdaptiveBehavior)GetValue(VerticalBehaviorProperty); }
+            set { SetValue(VerticalBehaviorProperty, value); }
+        }
+
+        public static readonly DependencyProperty VerticalBehaviorProperty =
+            DependencyProperty.Register("VerticalBehavior", typeof(AdaptiveBehavior), typeof(AdaptivePanel), new PropertyMetadata(AdaptiveBehavior.Standard, VerticalBehaviorCallback));
+
+        private static void VerticalBehaviorCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            var own = (AdaptivePanel)d;
+            own.InvalidateMeasure();
+            own.InvalidateArrange();
+
+        }
+
+        #endregion
+    }
+}
---
+++ AdaptivePanel/MainPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AdaptivePanel
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            MinimumWidthSlider.ValueChanged += MinimumWidthSlider_ValueChanged;
+            MaximumWidthSlider.ValueChanged += MaximumWidthSlider_ValueChanged;
+
+            MinimumHeightSlider.ValueChanged += MinimumHeightSlider_ValueChanged;
+            MaximumHeightSlider.ValueChanged += MaximumHeightSlider_ValueChanged;
+
+        }
+
+
+
+
+        #region Horizontal
+        private void RadioHorizontalBehavior_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = ((RadioButton)sender);
+            AdaptivePanel.HorizontalBehavior = (Huyn.AdaptiveBehavior)Enum.Parse(typeof(Huyn.AdaptiveBehavior), (string)radioButton.Tag);
+
+        }
+
+
+        private void HorizontalAlignmentRadio_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = ((RadioButton)sender);
+            AdaptivePanel.HorizontalAlignment = (HorizontalAlignment)Enum.Parse(typeof(HorizontalAlignment), (string)radioButton.Tag);
+        }
+
+        private void MinimumWidthCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            if (checkbox.IsChecked.Value)
+            {
+                MinimumWidthSlider.Visibility = Visibility.Visible;
+                AdaptivePanel.MinWidth = MinimumWidthSlider.Value;
+            }
+            else
+            {
+                MinimumWidthSlider.Visibility = Visibility.Collapsed;
+                AdaptivePanel.MinWidth = 0;
+            }
+        }
+
+        private void MinimumWidthSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            AdaptivePanel.MinWidth = e.NewValue;
+        }
+
+
+        private void MaximumWidthCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            if (checkbox.IsChecked.Value)
+            {
+                MaximumWidthSlider.Visibility = Visibility.Visible;
+                AdaptivePanel.MaxWidth = MaximumWidthSlider.Value;
+
+            }
+            else
+            {
+                MaximumWidthSlider.Visibility = Visibility.Collapsed;
+                AdaptivePanel.MaxWidth = double.PositiveInfinity;
+            }
+        }
+
+        private void MaximumWidthSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            AdaptivePanel.MaxWidth = e.NewValue;
+        }
+
+        #endregion
+
+
+        #region Vertical
+
+        private void RadioVerticalBehavior_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = ((RadioButton)sender);
+            AdaptivePanel.VerticalBehavior = (Huyn.AdaptiveBehavior)Enum.Parse(typeof(Huyn.AdaptiveBehavior), (string)radioButton.Tag);
+
+        }
+
+        private void VerticalAlignmentRadio_Click(object sender, RoutedEventArgs e)
+        {
+            var radioButton = ((RadioButton)sender);
+            AdaptivePanel.VerticalAlignment = (VerticalAlignment)Enum.Parse(typeof(VerticalAlignment), (string)radioButton.Tag);
+        }
+
+        private void MinimumHeightCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            if (checkbox.IsChecked.Value)
+            {
+                MinimumHeightSlider.Visibility = Visibility.Visible;
+                AdaptivePanel.MinHeight = MinimumHeightSlider.Value;
+            }
+            else
+            {
+                MinimumHeightSlider.Visibility = Visibility.Collapsed;
+                AdaptivePanel.MinHeight = 0;
+            }
+        }
+
+        private void MinimumHeightSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            AdaptivePanel.MinHeight = e.NewValue;
+        }
+
+
+        private void MaximumHeightCheckbox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            if (checkbox.IsChecked.Value)
+            {
+                MaximumHeightSlider.Visibility = Visibility.Visible;
+                AdaptivePanel.MaxHeight = MaximumHeightSlider.Value;
+
+            }
+            else
+            {
+                MaximumHeightSlider.Visibility = Visibility.Collapsed;
+                AdaptivePanel.MaxHeight = double.PositiveInfinity;
+            }
+        }
+
+        private void MaximumHeightSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            AdaptivePanel.MaxHeight = e.NewValue;
+        }
+
+        #endregion
+
+   
+    }
+}
